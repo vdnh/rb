8b594132908f31766c4c565d0d5129f5af29e0e2
admin <admin@server-gps>
2018-11-29 09:14:08 -0800

    Third Commit - formule registration

--- src/main/java/com/sprsecu/sprjwtangu/sec/SecurityConstants.java
+++ src/main/java/com/sprsecu/sprjwtangu/sec/SecurityConstants.java
@@ -8,6 +8,6 @@ public class SecurityConstants {
     public static final String SECRET = "vdnh@yahoo.com";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 240_000; //4 minutes
+    public static final long EXPIRATION_TIME = 2_400_000; //40 minutes
     //public static final long EXPIRATION_TIME = 864_000_000; //10 days
 }
