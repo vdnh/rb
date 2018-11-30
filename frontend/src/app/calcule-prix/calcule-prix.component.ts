a3287f6dc310bcb634eaf07311342889f05fa370
admin <admin@server-gps>
2018-11-30 13:16:33 -0800

    Sixth Commit - Calculation le prix 1st

--- frontend/src/app/calcule-prix/calcule-prix.component.ts
+++ frontend/src/app/calcule-prix/calcule-prix.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculePrixComponent implements OnInit {
 
+  mode=1; // en pouce et lbs    - mode = 2 on est en cm et kg
   // les details de marchandise
   longeur:number=0.00;
   largeur:number=0.00;
@@ -25,9 +26,38 @@ export class CalculePrixComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  changeUnite(){
+    if (this.mode==1){
+      this.mode=2; // pouce en cm 
+      this.poids=Math.round(this.poids / 2.2046);
+      this.longeur=Math.round(this.longeur / 0.39370);
+      this.largeur=Math.round(this.largeur / 0.39370);
+      this.hauteur=Math.round(this.hauteur / 0.39370);
+    }
+    else{
+      this.mode=1; // cm en pouce
+      this.poids=Math.round(this.poids * 2.2046);
+      this.longeur=Math.round(this.longeur * 0.39370);
+      this.largeur=Math.round(this.largeur * 0.39370);
+      this.hauteur=Math.round(this.hauteur * 0.39370);
+    }
+  }
   
+  // kg en lbs   ---    lb = kg * 2.2046
+  kgEnLbs(poids:number){
+    poids = poids * 2.2046;
+    console.log(poids);
+  }
+  // cm en pouce  ---   pouce = cm * 0.39370
+  cmEnPouce(longeur:number){
+    longeur = longeur * 0.39370;
+    console.log(longeur);
+  }
   // calcule le pointage de poids (en lbs)
-  poidsPointage(poids:number){
+  poidsPointage(poids:number, mode:number){
+    if (mode == 2)
+      poids = poids * 2.2046;
     if (poids>0 && poids<=10000)    
       return 1;
     if (poids>10000 && poids<=20000)    
@@ -42,7 +72,9 @@ export class CalculePrixComponent implements OnInit {
   }
 
   // calcule le pointage de longueur (en pouce)
-  longeurPointage(longeur:number){
+  longeurPointage(longeur:number, mode:number){
+    if (mode == 2)
+      longeur = longeur * 0.39370;
     if (longeur>0 && longeur<=120)    
       return 1;
     if (longeur>121 && longeur<=240)    
@@ -57,7 +89,9 @@ export class CalculePrixComponent implements OnInit {
   }
 
   // calcule le pointage de largeur (en pouce)
-  largeurPointage(largeur:number){
+  largeurPointage(largeur:number, mode:number){
+    if (mode == 2)
+      largeur = largeur * 0.39370;
     if (largeur>0 && largeur<=48)    
       return 1;
     if (largeur>48 && largeur<=102)    
@@ -72,7 +106,9 @@ export class CalculePrixComponent implements OnInit {
   }
 
   // calcule le pointage de hauteur (en pouce)
-  hauteurPointage(hauteur:number){
+  hauteurPointage(hauteur:number, mode:number){
+    if (mode == 2)
+      hauteur = hauteur * 0.39370;
     if (hauteur>0 && hauteur<=92)    
       return 1;
     if (hauteur>92 && hauteur<=120)    
@@ -104,8 +140,8 @@ export class CalculePrixComponent implements OnInit {
     return 0.00;
   }
   onOk(){    
-    this.totalPoints = this.longeurPointage(this.longeur) + this.largeurPointage(this.largeur) + this.hauteurPointage(this.hauteur) 
-      + this.poidsPointage(this.poids) + this.valeur + this.distance;
+    this.totalPoints = this.longeurPointage(this.longeur, this.mode) + this.largeurPointage(this.largeur, this.mode) + this.hauteurPointage(this.hauteur, this.mode) 
+      + this.poidsPointage(this.poids, this.mode) + this.valeur + this.distance;
     
     this.prix = this.prixDepart(this.totalPoints) + this.prixDistance(this.totalPoints) + this.prixToile(this.totalPoints)
       + this.prixAttendre(this.totalPoints) + this.prixSuplement(this.totalPoints, this.heurs_supl);
