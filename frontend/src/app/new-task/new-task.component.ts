418b0e56ac133e4d1af8db86e82d3b903070e7d8
User <User@User-PC>
2019-01-03 11:54:26 -0500

    03012019 a 11h30

--- frontend/src/app/new-task/new-task.component.ts
+++ frontend/src/app/new-task/new-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
@@ -7,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskComponent implements OnInit {
 
+  nrSelect = 46;
+  states = [
+    {name: 'Arizona', abbrev: 'AZ'},
+    {name: 'California', abbrev: 'CA'},
+    {name: 'Colorado', abbrev: 'CO'},
+    {name: 'New York', abbrev: 'NY'},
+    {name: 'Pennsylvania', abbrev: 'PA'},
+  ];
+  temp=this.states[4];
+  //*
+  form = new FormGroup({
+    state: new FormControl(this.states[3]),
+  });
+  //*/
   constructor() { }
 
   ngOnInit() {
