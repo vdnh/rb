2b7da73f9ec3684fbdb07e5d1f847c7e7b9d290c
admin <admin@server-gps>
2019-07-26 15:52:39 -0400

    Import SSL certificate for Spring Boot - OK

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
- 	res.sendFile(path.join(__dirname + '/dist/index.html'));
+// app.get('/*', function(req,res) {
+//  	res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+// app.listen(80);
+
+
+// Make sure to run the code using sudo as port 80 needs sudo access 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
+  res.end();
 });
-app.listen(80);
+
+server.listen(80);
+console.log(`http2https ==> 80:443`);
--- frontend/src/services/globals.ts
+++ frontend/src/services/globals.ts
@@ -3,7 +3,7 @@
 //
 'use strict';
 
-export const adServer: string="http://192.168.0.131";//"//192.168.0.131"; // adresse de server java - spring
+export const adServer: string="https://cts.sosprestige.com";//"//192.168.0.131"; // adresse de server java - spring
 export const host: string=adServer+":8080"; // adresse de server avec :8080
 export const hostUserInfo: string=host+"/users/"; // adresse de server avec  /users/
 export const version: string="1.0.0"; 
